2e1350c4b2680584348f466a31c443a74a1d14ed
Ishan Vyas <vyasishanatc194@gmail.com>
2019-02-05 20:23:51 +0530

    data

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<?php error_reporting(E_ALL & ~E_WARNING); ?>
+<html lang="en">
+<head>
+<title>Bootstrap 4 Website Example</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+<script src="script.js"></script>
+<style>
+    .fakeimg {
+    height: 200px;
+    background: #aaa;
+    }
+    *{ margin:0; }
+    .loader {
+        border: 5px solid #f3f3f3;
+        border-radius: 50%;
+        border-top: 5px solid #000;
+        width: 120px;
+        height: 120px;
+        -webkit-animation: spin 2s linear infinite; /* Safari */
+        animation: spin 2s linear infinite;
+    }
+  
+    /* Safari */
+    @-webkit-keyframes spin {
+        0% { -webkit-transform: rotate(0deg); }
+        100% { -webkit-transform: rotate(360deg); }
+    }
+  
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+    .backface-div {
+            display: flex;
+            height: 100vh;
+            width: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            justify-content: center;
+            align-items: center;
+            position:fixed;
+        }
+</style>
+</head>
+<body>
+
+
+
+<div class="container-fiuld" style="margin-top:30px">
+    <div class="row">
+        <div class="col-sm-4">
+                <div id="image_preview">
+                <?php if (file_exists('output.png')) 
+                    { ?>
+                                    <img id="previewing" width="80%" height="80%" src="<?php echo 'http://'.$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI].'/'.'output.png'?>" />
+                                    <a href="<?php echo 'http://'.$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI].'/'.'output.png'?>" download>Download</a>
+
+                    <?php } else {?>
+                        <img id="previewing" width="80%" height="90%" src="<?php echo 'http://'.$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI].'/'.'template.png'?>" />
+                    <?php } ?>
+                </div>
+
+        </div>
+        <div class="col-sm-4">
+            <h3>Layout & Text</h3><br/>
+            <hr>
+            <form id="uploadimage" action="" method="post" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="file">Screen Shot:</label>
+                    <input type="file" name="screen_shot" id="screen_shot"  />
+                </div>
+                <div class="form-group">
+                    <label for="file">Background Color</label>
+                    <input type="color" name="background_color" id="background_color" value="#FF5A5F" />
+                </div>
+                <div class="form-group">
+                    <label for="file">Background Image:</label>
+                    <input type="file" name="background_image" id="background_image"  />
+                </div>
+                <div class="form-group">
+                    <label for="file">Text Content:</label>
+                    <input type="text" name="text_content" id="text_content"  />
+                </div>
+                <div class="form-group">
+                    <label for="file">Text Size</label>
+                    <input type="number" name="text_size" id="text_size" min="1" />
+                </div>
+                <div class="form-group">
+                    <label for="file">Text Color</label>
+                    <input type="color" name="text_color" id="text_color" value="#FFFFFF" />
+                </div>
+                <input type="submit" value="Submit" class="submit" />
+            </form>
+        </div>     
+        <div class="col-sm-4">
+            <div id="message"></div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,154 @@
+$(document).ready(function (e) {
+    $("#loader").hide();
+$("#uploadimage").on('submit',(function(e) {
+    e.preventDefault();
+    $("#message").empty();
+   
+    $.ajax({
+    url: "ajax_php_file.php", // Url to which the request is send
+    type: "POST",             // Type of request to be send, called as method
+    data: new FormData(this), // Data sent to server, a set of key/value pairs (i.e. form fields and values)
+    contentType: false,       // The content type used when sending data to the server.
+    cache: false,             // To unable request pages to be cached
+    processData:false,  
+    beforeSend: function() {
+        $("#loader").show();
+     },      // To send DOMDocument or non processed data file it is set to false
+    success: function(data)   // A function to be called if request succeeds
+    {
+        setTimeout(function(){
+            $('#previewing').attr('src','output.png');
+            $('#loader').hide();
+        },1000);
+
+        //window.location.reload(true);
+
+    },
+    error:function(data)
+    {
+        setTimeout(function(){
+            $('#previewing').attr('src','output.png');
+            $('#loader').hide();
+        },1000);
+    }
+    });
+}));
+
+    /*$("#screen_shot").change(function() {
+    
+        $("#message").empty(); // To remove the previous error message
+        var file = this.files[0];
+        var imagefile = file.type;
+        var match= ["image/jpeg","image/png","image/jpg"];
+        if(!((imagefile==match[0]) || (imagefile==match[1]) || (imagefile==match[2])))
+        {
+            $('#previewing').attr('src','noimage.png');
+            $("#message").html("<p id='error'>Please Select A valid Image File</p>"+"<h4>Note</h4>"+"<span id='error_message'>Only jpeg, jpg and png Images type allowed</span>");
+            return false;
+        }
+        else
+        { 
+            var file_data = $("#screen_shot").prop("files")[0];
+        
+            var form_data = new FormData();
+            form_data.append("screen_shot", file_data);
+          
+            $.ajax({
+                url: "ajax_php_file.php", // Url to which the request is send
+                type: "POST",   
+                data: form_data,
+                cache: false,
+                contentType: false,
+                processData: false,
+                beforeSend: function() {
+                    $("#loader").show();
+                },      // To send DOMDocument or non processed data file it is set to false
+                success: function(data)   // A function to be called if request succeeds
+                {    $("#message").html(data);
+                
+                    window.location.reload(true);
+            
+                },
+                complete:function(data){
+                    // Hide image container
+                    $('#loader').hide();
+                }
+            
+            });
+        }
+    });
+
+    $("#background_color").change(function(){
+        var form_data = new FormData();
+            form_data.append("background_color",$(this).val());
+          
+            $.ajax({
+                url: "ajax_php_file.php", // Url to which the request is send
+                type: "POST",   
+                data: form_data,
+                cache: false,
+                contentType: false,
+                processData: false,
+                beforeSend: function() {
+                    $("#loader").show();
+                },      // To send DOMDocument or non processed data file it is set to false
+                success: function(data)   // A function to be called if request succeeds
+                {    $("#message").html(data);
+                     
+                    //window.location.reload(true);
+            
+                },
+                complete:function(data){
+                    // Hide image container
+                    $('#loader').hide();
+                }
+            
+            });
+       
+    });
+
+    $("#background_image").change(function() {
+    
+        $("#message").empty(); // To remove the previous error message
+        var file = this.files[0];
+        var imagefile = file.type;
+        var match= ["image/jpeg","image/png","image/jpg"];
+        if(!((imagefile==match[0]) || (imagefile==match[1]) || (imagefile==match[2])))
+        {
+            $('#previewing').attr('src','noimage.png');
+            $("#message").html("<p id='error'>Please Select A valid Image File</p>"+"<h4>Note</h4>"+"<span id='error_message'>Only jpeg, jpg and png Images type allowed</span>");
+            return false;
+        }
+        else
+        { 
+            var file_data = $("#background_image").prop("files")[0];
+        
+            var form_data = new FormData();
+            form_data.append("background_image", file_data);
+          
+            $.ajax({
+                url: "ajax_php_file.php", // Url to which the request is send
+                type: "POST",   
+                data: form_data,
+                cache: false,
+                contentType: false,
+                processData: false,
+                beforeSend: function() {
+                    $("#loader").show();
+                },      // To send DOMDocument or non processed data file it is set to false
+                success: function(data)   // A function to be called if request succeeds
+                {    $("#message").html(data);
+                
+                    //window.location.reload(true);
+            
+                },
+                complete:function(data){
+                    // Hide image container
+                    $('#loader').hide();
+                }
+            
+            });
+        }
+    });*/
+
+});
